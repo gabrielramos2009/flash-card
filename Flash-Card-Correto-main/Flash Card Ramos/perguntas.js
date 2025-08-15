a76ea56d62f13bf7b4f1d5e612299f750439c52d
gabrielramos2009 <bielramoa2@gmail.com>
2025-08-15 09:47:57 -0300

    Add files via upload

---
+++ Flash-Card-Correto-main/Flash Card Ramos/perguntas.js
@@ -0,0 +1,18 @@
+criarCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual é a capital do Paraná',
+    'A capital do Paraná',
+    'A capital do Paraná é Curitiba'
+)
+
+criarCartao(
+    'Programação',
+    'O que é Python',
+    'O Python é uma linguagem de programção'
+)
